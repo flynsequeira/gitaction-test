95b917b67f28699f974ff38dfdb1ce576f911f02
Flyn Sequeira <flyns3694@gmail.com>
2025-03-31 18:13:04 -0500

    Update script.py to test both env

--- script.py
+++ script.py
@@ -2,4 +2,5 @@ import os
 
 # Retrieve the environment variable; if it's not set, show a default message.
 test_env = os.getenv('TEST_ENV_VAR', 'Variable not found')
+testing2 = os.getenv('TEST2', 'No env var for test 2')
 print(f"TEST_ENV_VAR: {test_env}")
